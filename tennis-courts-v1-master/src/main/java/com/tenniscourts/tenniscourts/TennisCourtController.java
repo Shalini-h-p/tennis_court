85941982b8b5c95b3edd4dd2eecf276db92baf42
weijie <a00279875@student.ait.ie>
2021-11-02 17:40:34 +0000

    tennis-court-task-1-2

---
+++ tennis-courts-v1-master/src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
@@ -0,0 +1,30 @@
+package com.tenniscourts.tenniscourts;
+
+import com.tenniscourts.config.BaseRestController;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class TennisCourtController extends BaseRestController {
+
+    private final TennisCourtService tennisCourtService;
+
+    @RequestMapping(value = "addTennisCourt",method = RequestMethod.POST )
+    public ResponseEntity<Void> addTennisCourt(TennisCourtDTO tennisCourtDTO) {
+        return ResponseEntity.created(locationByEntity(tennisCourtService.addTennisCourt(tennisCourtDTO).getId())).build();
+    }
+
+    @RequestMapping(value = "findTennisCourtById",method = RequestMethod.GET)
+    public ResponseEntity<TennisCourtDTO> findTennisCourtById(Long tennisCourtId) {
+        return ResponseEntity.ok(tennisCourtService.findTennisCourtById(tennisCourtId));
+    }
+
+    @RequestMapping(value = "findTennisCourtWithSchedulesById",method = RequestMethod.GET)
+    public ResponseEntity<TennisCourtDTO> findTennisCourtWithSchedulesById(Long tennisCourtId) {
+        return ResponseEntity.ok(tennisCourtService.findTennisCourtWithSchedulesById(tennisCourtId));
+    }
+}
---
+++ tennis-courts-v1-master/src/main/java/com/tenniscourts/tenniscourts/TennisCourtMapper.java
@@ -0,0 +1,13 @@
+package com.tenniscourts.tenniscourts;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TennisCourtMapper {
+
+    TennisCourtDTO map(TennisCourt source);
+
+    @InheritInverseConfiguration
+    TennisCourt map(TennisCourtDTO source);
+}
